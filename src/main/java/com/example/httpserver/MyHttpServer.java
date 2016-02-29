9dd7040cb657c7570c59223da8d29b9a0371cfa6
xiongwenwu <xiongwenwu1997@126.com>
2016-02-29 16:14:20 +0800

    httpserver实现和任务分工

---
+++ src/main/java/com/example/httpserver/MyHttpServer.java
@@ -0,0 +1,60 @@
+package com.example.httpserver;
+
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.text.SimpleDateFormat;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.spi.HttpServerProvider;
+
+/**
+ * @project SimpleHttpServer
+ * @author 
+ * @vresion 1.0 
+ * @description  自定义的http服务器
+ * 我们需要提供http服务，以便于网页可以展示一些东西，所以需要用到web服务器，
+ * java web一般情况下都是用tomcat做服务器，但是我们这里不用这种玩意，自己用jdk自带的包简单实现一个，
+ * 也可以用jetty这种极其轻量的包，这里使用自带的com.sun.net.httpserver.HttpServer
+ */
+public class MyHttpServer {
+    //启动服务，监听来自客户端的请求
+	public static void httpserverService() throws IOException {
+		HttpServerProvider provider = HttpServerProvider.provider();
+		HttpServer httpserver =provider.createHttpServer(new InetSocketAddress(6666), 100);//监听端口6666,能同时接 受100个请求
+		httpserver.createContext("/myApp", new MyHttpHandler()); 
+		httpserver.setExecutor(null);
+		httpserver.start();
+		System.out.println("server started");
+	}
+	//Http请求处理类
+	static class MyHttpHandler implements HttpHandler {
+		public void handle(HttpExchange httpExchange) throws IOException {
+			SimpleDateFormat time=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+			String TimeString = time.format(new java.util.Date());
+			String responseMsg = "ok " + TimeString;   //响应信息
+			InputStream in = httpExchange.getRequestBody(); //获得输入流
+			BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+			String temp = null;
+			while((temp = reader.readLine()) != null) {
+				System.out.println("client request:"+temp);
+			}
+			httpExchange.sendResponseHeaders(200, responseMsg.length()); //设置响应头属性及响应信息的长度
+			OutputStream out = httpExchange.getResponseBody();  //获得输出流
+			out.write(responseMsg.getBytes());
+			out.flush();
+			httpExchange.close();                               
+			
+		}
+	}
+	public static void main(String[] args) throws IOException {
+		httpserverService();
+	}
+}
---
+++ src/main/java/com/example/httpserver/Test.java
@@ -0,0 +1,70 @@
+package com.example.httpserver;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @project SimpleHttpServer
+ * @author 
+ * @vresion 1.0 
+ * @description 测试类  
+ */
+public class Test {
+	public static void main(String[] args) {
+		
+		ExecutorService exec = Executors.newCachedThreadPool();
+		// 测试并发20个请求对MyHttpServer的影响
+		for (int i = 0; i < 20; i++) {
+			Runnable run = new Runnable() {
+				public void run() {
+					try {
+						startWork();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			};
+			exec.execute(run);
+		}
+		exec.shutdown();// 关闭线程池
+	}
+
+	public static void startWork() throws IOException {
+		SimpleDateFormat time=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		String TimeString = time.format(new java.util.Date());
+		//System.out.println(TimeString);
+		URL url = new URL("http://127.0.0.1:6666/myApp");
+		HttpURLConnection urlConn = (HttpURLConnection) url.openConnection();
+		urlConn.setDoOutput(true);
+		urlConn.setDoInput(true);
+		urlConn.setRequestMethod("POST");
+		// 测试内容包
+		String teststr = "this is a test message " + TimeString;
+		OutputStream out = urlConn.getOutputStream();
+		out.write(teststr.getBytes());
+		out.flush();
+		while (urlConn.getContentLength() != -1) {
+			if (urlConn.getResponseCode() == 200) {
+				InputStream in = urlConn.getInputStream();
+				BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+				String temp = "";
+				while ((temp = reader.readLine()) != null) {
+					System.err.println("server response:" + temp);// 打印收到的信息
+				}
+				reader.close();
+				in.close();
+				urlConn.disconnect();
+				break;
+			}
+		}
+	}
+}
